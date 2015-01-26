4ff89eaa219220eaac8855f480c9af17d3180c2c
Christian Hoffmeister <mail@choffmeister.de>
2015-01-26 07:24:13 +0100

    Allow dynamic changing of searched tag

--- index.js
+++ index.js
@@ -1,38 +1,62 @@
 var util = require('util'),
+    _ = require('lodash'),
     RestClient = new require('node-rest-client').Client;
 
 var config = {
   giphy: {
     url: 'http://api.giphy.com/v1/gifs/random?tag=%s&api_key=%s',
     token: 'dc6zaTOxFJmzC', // public beta token
-    search: 'family+guy' // what to search giphy for
+    defaultTag: 'homer+simpson' // what to search giphy for,
   },
   hipChat: {
-    url: 'https://api.hipchat.com/v2/room/%s/notification?auth_token=%s',
+    retrieveUrl: 'https://api.hipchat.com/v2/room/%s/history/latest?auth_token=%s',
+    notificationUrl: 'https://api.hipchat.com/v2/room/%s/notification?auth_token=%s',
     token: '', // fill
     room: '' // fill
   }
 };
 
+var parseCommands = function (hipChatMessages) {
+  return _.chain(hipChatMessages)
+    .map(function (x) { return x.message })
+    .filter(function (x) { return x.length > 0 && x.charAt(0) == '/' })
+    .map(function (x) {
+      var parts = x.split(/ {1,}/g);
+      return {
+        command: parts[0].slice(1),
+        arguments: parts.slice(1)
+      }
+    })
+    .value();
+};
+
+var getTag = function (hipChatCommands) {
+  var lastTagCommand = _.chain(hipChatCommands).filter(function (x) { return x.command == 'tag' }).last().value();
+  return lastTagCommand ? lastTagCommand.arguments.join('+') : config.giphy.defaultTag;
+};
+
 var client = new RestClient();
-var giphyUrl = util.format(config.giphy.url, config.giphy.search, config.giphy.token);
-var hipChatUrl = util.format(config.hipChat.url, config.hipChat.room, config.hipChat.token);
+client.get(util.format(config.hipChat.retrieveUrl, config.hipChat.room, config.hipChat.token), function (hipChatRetrieveData, hipChatRetrieveRes) {
+  var commands = parseCommands(hipChatRetrieveData.items);
+  var tag = getTag(commands);
 
-client.get(giphyUrl, function(giphyData, giphyRes) {
-  var messageArgs = {
-    data: {
-      'color': 'random',
-      'message': giphyData.data.image_original_url,
-      'notify': false,
-      'message_format': 'text'
-    },
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }
-  };
+  client.get(util.format(config.giphy.url, tag, config.giphy.token), function(giphyData, giphyRes) {
+    var messageText = giphyData.data.image_original_url || 'Could not find any GIF for tag ' + tag;
+    var messageArgs = {
+      data: {
+        'color': 'random',
+        'message': messageText,
+        'notify': false,
+        'message_format': 'text'
+      },
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    };
 
-  client.post(hipChatUrl, messageArgs, function (hipChatData, hipChatRes) {
-    // do nothing
+    client.post(util.format(config.hipChat.notificationUrl, config.hipChat.room, config.hipChat.token), messageArgs, function (hipChatNotificationData, hipChatNotificationRes) {
+      // do nothing
+    });
   });
 });
