2070d7a8c9434b875fc14e560c83767b12a52536
Christian Hoffmeister <mail@choffmeister.de>
2015-01-26 06:57:34 +0100

    Use Giphy random endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var util = require('util'),
 
 var config = {
   giphy: {
-    url: 'http://api.giphy.com/v1/gifs/search?q=%s&api_key=%s',
+    url: 'http://api.giphy.com/v1/gifs/random?tag=%s&api_key=%s',
     token: 'dc6zaTOxFJmzC', // public beta token
     search: 'family+guy' // what to search giphy for
   },
@@ -14,23 +14,15 @@ var config = {
   }
 };
 
-var random = function (arr) {
-  var length = arr.length;
-  var index = Math.floor(Math.random() * length);
-  return arr[index];
-};
-
 var client = new RestClient();
 var giphyUrl = util.format(config.giphy.url, config.giphy.search, config.giphy.token);
 var hipChatUrl = util.format(config.hipChat.url, config.hipChat.room, config.hipChat.token);
 
-client.get(giphyUrl, function(giphyData, giphyRes){
-  var imageUrl = random(giphyData.data).images.original.url;
-
+client.get(giphyUrl, function(giphyData, giphyRes) {
   var messageArgs = {
     data: {
       'color': 'random',
-      'message': imageUrl,
+      'message': giphyData.data.image_original_url,
       'notify': false,
       'message_format': 'text'
     },
